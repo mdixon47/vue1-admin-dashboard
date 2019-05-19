a15a0b4b07491b5e5c7810f39768a5d5f353dc20
Malik Dixon <malik.dixon47@gmail.com>
2019-05-19 10:09:13 -0600

    First commit

--- src/router.js
+++ src/router.js
@@ -1,26 +1,40 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import SignIn from "./views/SignInFlow/SignIn.vue";
+import Request from "./views/SignInFlow/Request.vue";
+import Recover from "./views/SignInFlow/Recover.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: About
+        },
+        {
+            path: "/signin",
+            name: "signin",
+            component: SignIn
+        },
+        {
+            path: "/request",
+            name: "request",
+            component: Request
+        },
+        {
+            path: "/recover",
+            name: "recover",
+            component: Recover
+        }
+    ]
 });
